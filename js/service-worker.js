a23c2fc1f1caa24bb294bfa77f6de9651aa8b49d
Braian Barrios <braianb105@alu.bue.edu.ar>
2024-08-08 23:59:03 -0300

    Initial commit

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,21 +1,42 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
+  event.waitUntil(
       caches.open('static-v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/app.js'
-        ]);
+          return cache.addAll([
+              '/',
+              '/index.html',
+              '/css/style.css', 
+              '/js/app.js'     
+          ]);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
+  );
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
       caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+          if (response) {
+              return response; 
+          }
+          return fetch(event.request).catch(() => {
+              
+              return caches.match('/index.html');
+          });
       })
-    );
-  });
-  
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['static-v1'];
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cacheName) => {
+                  if (!cacheWhitelist.includes(cacheName)) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
+  );
+});
